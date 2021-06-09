527cfe14171146488850765f1d12826d87eb6430
henryhaulka <onuhenry24@gmail.com>
2021-06-10 00:07:28 +0100

    add username to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_222032) do
+ActiveRecord::Schema.define(version: 2021_06_09_225628) do
 
   create_table "users", force: :cascade do |t|
     t.string "encrypted_password", default: "", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_222032) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "Username"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

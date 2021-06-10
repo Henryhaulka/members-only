86fadc973bb564f87c51549a0fbbc0bebd171f0e
henryhaulka <onuhenry24@gmail.com>
2021-06-10 14:23:00 +0100

    update files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,25 @@ class PostsController < ApplicationController
         @posts = Post.all
     end
 
+    def show
+        @post = Post.find(params[:id])
+    end
+
     def new
         @post= Post.new
     end
 
     def create
-        @post = Post.create 
+        @post = Post.create(use_params)
+        if @post.save
+            redirect_to @post
+        else
+            render :new
+        end
+    end
+
+    private
+    def use_params
+        params.require(:post).permit(:title, :body, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,10 @@ ActiveRecord::Schema.define(version: 2021_06_10_082726) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+   
   end
 
   create_table "users", force: :cascade do |t|
